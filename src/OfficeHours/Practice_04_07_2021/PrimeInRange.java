c04896e2ac9ea0655202388ce7eb4e2dd64d43d2
saimCybertek <saim@cybertekschool.com>
2021-04-07 17:55:54 -0500

    Office hours April 7

---
+++ src/OfficeHours/Practice_04_07_2021/PrimeInRange.java
@@ -0,0 +1,40 @@
+package OfficeHours.Practice_04_07_2021;
+
+public class PrimeInRange {
+
+    public static void main(String[] args) {
+
+        /*
+        [IQ] Prime in range
+        Given a number. Print out all the prime numbers from 2 to that number A prime number is a number that is only divisible by 1 and itself.
+        Ex:
+        Input: 50
+        Output:
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47
+         */
+
+        int number = 50;
+
+        // 5/2, 5/3, 5/4
+
+        for(int j = 2; j <= number; j++) {
+            boolean isPrime = true;
+
+            int checkNumber = j; // 5
+
+            for(int i = 2; i < checkNumber; i++){
+                if(checkNumber % i == 0) { // 5 % 2 , 5 % 3, 5 % 4
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime) {
+                System.out.print(checkNumber  + " ");
+            }
+
+        }
+
+
+    }
+}
