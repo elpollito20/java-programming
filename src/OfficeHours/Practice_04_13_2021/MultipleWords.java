6b75937b8740f231a67cc86d1b82ae148aa92067
saimCybertek <saim@cybertekschool.com>
2021-04-13 17:53:10 -0500

    Office hours April 13

---
+++ src/OfficeHours/Practice_04_13_2021/MultipleWords.java
@@ -0,0 +1,20 @@
+package OfficeHours.Practice_04_13_2021;
+
+import java.util.Arrays;
+
+public class MultipleWords {
+    public static void main(String[] args) {
+        /*
+        "knife, wooden spoons, plates, cups, forks, pan, pot, trash can, fridge, dish washer”
+         */
+        String words = "knife, wooden spoons, plates, cups, forks, pan, pot, trash can, fridge, dish washer";
+        String [] arrWords = words.split(", ");
+
+        for(String eachWord : arrWords){
+            if(eachWord.contains(" ")){
+                System.out.println(eachWord);
+            }
+        }
+
+    }
+}
