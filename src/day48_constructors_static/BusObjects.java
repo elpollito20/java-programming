fdfdfd774bc0efce82af11599061b1fb5b31643b
CybertekSchool <github@cybertekschool.com>
2021-05-29 17:06:01 -0400

    static method demo using calculator

--- src/day48_constructors_static/BusObjects.java
+++ src/day48_constructors_static/BusObjects.java
@@ -7,5 +7,14 @@ public class BusObjects {
         bus.engine = new Engine(10);
 
         System.out.println(bus.toString());
+
+        Bus metroBus = new Bus();
+        metroBus.driver = new Driver("Aziz");
+        metroBus.engine = new Engine(15);
+
+        System.out.println(metroBus);
+
+        //print Driver name of metroBus
+        System.out.println(metroBus.driver.getName());
     }
 }
