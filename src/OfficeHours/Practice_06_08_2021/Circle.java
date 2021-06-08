d6e6a36ec5d1d11d99932f0adb1f55488981a598
saimCybertek <57418084+saimCybertek@users.noreply.github.com>
2021-06-08 17:27:42 -0500

    Office hours  june 8

---
+++ src/OfficeHours/Practice_06_08_2021/Circle.java
@@ -0,0 +1,7 @@
+package OfficeHours.Practice_06_08_2021;
+
+public class Circle extends Shape{
+
+
+
+}
---
+++ src/OfficeHours/Practice_06_08_2021/ShapeObjects.java
@@ -0,0 +1,27 @@
+package OfficeHours.Practice_06_08_2021;
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+
+        Shape shape1 = new Shape();
+        shape1.calculateArea();
+
+        Square squareOne = new Square();
+        squareOne.side = 5;
+        squareOne.calculateArea();
+        squareOne.calculatePerimeter();
+        System.out.println(squareOne);
+
+        Rectangle rectangle1 = new Rectangle();
+        rectangle1.width = 3;
+        rectangle1.length = 4;
+        rectangle1.calculateArea();
+        rectangle1.calculatePerimeter();
+        System.out.println(rectangle1);
+
+        Circle circle1 = new Circle();
+
+
+    }
+}
