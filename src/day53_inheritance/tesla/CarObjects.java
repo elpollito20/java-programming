920499d19fd749dbfbe6497c572de0b211bd4278
CybertekSchool <github@cybertekschool.com>
2021-06-13 12:08:25 -0400

    - added Roadster.java and called super constructor

--- src/day53_inheritance/tesla/CarObjects.java
+++ src/day53_inheritance/tesla/CarObjects.java
@@ -19,6 +19,13 @@ public class CarObjects {
             System.out.println("purchasing " + ec1.toString());
         }
 
+        System.out.println(ElectricCar.getCount());
+
+        ElectricCar ec2 = new ElectricCar("Tesla", "CyberTruck", 59900, 2022, 300);
+        System.out.println(ec2.toString());
+        System.out.println("Count = " + ec2.getCount()); //call static method using object
+        System.out.println("Count = " + ElectricCar.getCount());//call static method using Classname
+
 
 
     }
