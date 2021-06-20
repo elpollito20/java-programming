a6714c2f01f2cef9d9f1ae4f11555ddba7fd2d60
CybertekSchool <github@cybertekschool.com>
2021-06-20 14:47:31 -0400

    - Flag drawing abstraction practice

---
+++ src/day55_abstraction/flags/FlagDrawing.java
@@ -0,0 +1,8 @@
+package day55_abstraction.flags;
+
+public class FlagDrawing {
+    public static void main(String[] args) {
+        RussianFlag russianFlag = new RussianFlag();
+        russianFlag.draw();
+    }
+}
